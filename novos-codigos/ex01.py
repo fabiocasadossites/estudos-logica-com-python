7f3443f416ea3a0f91c0125f27a831a949f3f98e
Fabio Augusto <44373172+fabiocasadossites@users.noreply.github.com>
2024-06-06 13:03:14 -0300

    Update ex01.py
    Feito o código destrito acima

--- novos-codigos/ex01.py
+++ novos-codigos/ex01.py
@@ -1,1 +1,12 @@
 # Neste algoritmo, crie uma variável que armazene uma string e uma lista que armazena várias strings.
+
+# Cria uma variável para armazenar uma string
+string_unico = "Exemplo de uma string única"
+
+# Cria uma lista para armazenar várias strings
+lista_strings = ["Primeira string", "Segunda string", "Terceira string"]
+
+# Imprime a string única e a lista de strings
+print("String única:", string_unico)
+print("Lista de strings:", lista_strings)
+
